3880807597eaf04c8f0941561e9dac6a0d4d9aa2
Zizoubrown <zizumoha2015@gmail.com>
2020-07-07 07:51:58 +0300

    move file to root directory

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+Import os
+
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?{}apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
@@ -15,3 +19,8 @@ class DevConfig(Config):
     Development configuration child class
     '''
     DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
