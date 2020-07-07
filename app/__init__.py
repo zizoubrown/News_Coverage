4c61a796c507b4e94849060a947b3f8dfbcd564e
Zizoubrown <zizumoha2015@gmail.com>
2020-07-07 08:57:30 +0300

    import configure request function

--- app/__init__.py
+++ app/__init__.py
@@ -15,13 +15,13 @@ bootstrap = Bootstrap()
 
 def create_app(config_name):
 
-    app Flask(__name__)
+     # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
-
-    #initializing flask extensionss
-    bootstrap.init_app(app)
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
     retun app
 
