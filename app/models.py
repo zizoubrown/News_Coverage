20ae111f7511dcc48d9f038b4ca4468a86a8c458
Zizoubrown <zizumoha2015@gmail.com>
2020-07-07 08:46:49 +0300

    add models.py and move news class

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class News:
+    '''
+    News class to define News objects
+    '''
+
+    def __init__(self):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.image_url = image_url
+        self.published_at = published_at
