090e5d1a0b3165ccbd5b2b85068000cd9f0388e4
Zizoubrown <zizumoha2015@gmail.com>
2020-07-07 08:54:10 +0300

    add configure request function

--- app/request.py
+++ app/request.py
@@ -5,10 +5,15 @@ from  .models import news
 News = news.News
 
 #Getting api key
-api_key = app.config['NEWS_API_KEY']
+api_key = None
 
 # Getting the news base url
-base_url = app.config['NEWS_API_BASE_URL']
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.cofig['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
 
 def get_everything(category):
     '''
