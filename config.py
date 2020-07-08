c756e336e8d8eaf60ef1cbf8c7649edf391e02bf
Zizoubrown <zizumoha2015@gmail.com>
2020-07-08 12:50:07 +0300

    debug

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?{}apiKey={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
