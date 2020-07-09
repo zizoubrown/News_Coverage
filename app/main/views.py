0092e56ffaa6ed98f8ef833fe5f2b091274219aa
Zizoubrown <zizumoha2015@gmail.com>
2020-07-09 18:41:58 +0300

    debug

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,5 @@
 from flask import render_template,request,redirect,url_for
 from .import main
-#from ..request import get_news,get_everything,search_news
 from ..request import get_news,get_article
 from ..models import Articles,News
 
@@ -26,6 +25,7 @@ def news_news(id):
     results = get_article(id)
     return render_template('news.html', data = results)
 
+
 @main.route('/search/<news_name>')
 def search(news_name):
     '''
@@ -37,25 +37,3 @@ def search(news_name):
     title = f'search results for {news_name}'
     return render_template('search.html',news=searched_news)
 
-
-#Views
-@main.route('/')
-def index():
-    '''
-    View root page function that returns the index page and it's data
-    '''
-    # getting general news sources
-    general_news = get_news()
-    title ='News Highlights - Homepage'
-    return render_template('index.html', title = title, new = general_news)
-
-
-@main.route('/news/<id>')
-def news_news(id):
-
-    '''
-    View news page function that returns the news details page and its data
-    '''
-
-    results = get_article(id)
-    return render_template('news.html', data = results)
