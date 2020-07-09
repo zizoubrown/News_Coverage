d360162b65016435bbaa58f296fd35e9fbf16d69
Zizoubrown <zizumoha2015@gmail.com>
2020-07-09 18:09:30 +0300

    add changes to views.py

--- app/main/views.py
+++ app/main/views.py
@@ -4,39 +4,27 @@ from .import main
 from ..request import get_news,get_article
 from ..models import Articles,News
 
-'''
 #Views
 @main.route('/')
 def index():
-
-    #add docstring quotes here
+    '''
     View root page function that returns the index page and it's data
-    #add docstring quotes here
+    '''
+    # getting general news sources
+    general_news = get_news()
+    title ='News Highlights - Homepage'
+    return render_template('index.html', title = title, new = general_news)
 
-    # Getting news artiles
-    news_articles = get_everything('bbc-news')
-    #top_headlines_news = get_everything('headlines')
-    #latest_news = get_everything('latest')
 
-    title = 'Home - Welcome to the best news website online'
+@main.route('/news/<id>')
+def news_news(id):
 
-    search_news = request.args.get('news_query')
+    '''
+    View news page function that returns the news details page and its data
+    '''
 
-    if search_news:
-        return redirect(url_for('main.search',news_name=search_news))
-    else:
-        return render_template('index.html', title=title, article=news_articles) #headlines=top_headlines_news, latest=latest_news)
-
-@main.route('/news/<news_id>')
-def news(news_id):
-
-    #add docstring quotes here
-    View news page function that returns the specific news details page and its data
-    #add docstring quotes here
-    news = get_news(id)
-    title = f'{news.title}'
-    return render_template('news.html',title=title, news=news)
-'''
+    results = get_article(id)
+    return render_template('news.html', data = results)
 
 @main.route('/search/<news_name>')
 def search(news_name):
@@ -49,7 +37,7 @@ def search(news_name):
     title = f'search results for {news_name}'
     return render_template('search.html',news=searched_news)
 
-#############Testing#######################
+
 #Views
 @main.route('/')
 def index():
